815a1d3a65007ecafd6e87b719e99897f09d30ad
jasmin <jasminchakma895@gmail.com>
2023-11-06 12:05:18 +0600

    get featured foods

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkafzub.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkafzub.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,20 +27,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const foodCollection = client.db('FeedX').collection('foodCollection');
+    const foodCollection = client.db("FeedX").collection("foodCollection");
 
-    app.get('/foods', async(req, res) => {
-        const result = await foodCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/foods", async (req, res) => {
+      const result = await foodCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/featured-foods", async (req, res) => {
+      const result = await foodCollection
+        .find()
+        .sort({ foodQuantity : -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
-    app.get('/foods/:id', async(req, res) => {
+    app.get("/foods/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await foodCollection.findOne(query);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
